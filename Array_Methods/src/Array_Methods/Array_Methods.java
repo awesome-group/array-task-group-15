a1a59aa5be91b066c49f10a6f9cd06c9167ef5e0
Vaqif Dadaszade <vaqifdadaszade012@gmail.com>
2022-05-28 10:35:34 +0400

    Complate Arrays Methods

---
+++ Array_Methods/src/Array_Methods/Array_Methods.java
@@ -0,0 +1,22 @@
+package Array_Methods;
+
+import java.util.Arrays;
+
+public class Array_Methods {
+
+	public static void main(String[] args) {
+
+		int[] arr = new int[] { 1, 2, 5, 6, 7, 8, 4 };
+		int[] arr2 = new int[] { 1, 2, 4, 5, 8, 6 };
+		int[] arr3=new int[arr.length];
+		System.out.println(Arrays.binarySearch(arr, 5));
+		System.out.println(Arrays.toString(arr2));
+		System.out.println(Arrays.toString(Arrays.copyOf(arr2, 3)));
+		System.out.println(Arrays.toString(Arrays.copyOfRange(arr2, 1, 5)));
+		System.out.println(Arrays.equals(arr, arr2));
+		Arrays.fill(arr, 10);
+		Arrays.toString(arr=arr3);
+
+	}
+
+}
